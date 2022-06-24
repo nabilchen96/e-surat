7629074de20f366925191ecb0e02fe1479e5822d
nabilchen96 <escendol900@gmail.com>
2022-06-24 09:28:45 +0700

    update

--- include/menu.php
+++ include/menu.php
@@ -133,7 +133,7 @@
 
         <!-- Menu on large screen START -->
         <ul class="center hide-on-med-and-down" id="nv">
-            <li><a href="./" class="ams hide-on-med-and-down"><i class="material-icons md-36">drafts</i> SIDLAP</a></li>
+            <li><a href="./" class="ams hide-on-med-and-down"><i class="material-icons md-36">drafts</i> SILDDAP</a></li>
             <li><div class="grs"></></li>
             <li><a href="./"><i class="material-icons"></i>&nbsp; Beranda</a></li>
             
